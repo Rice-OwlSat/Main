0ea9bfe82d5257c3aa06cec763d200222a2fe5b7
Clay G <buzzscoutbark@gmail.com>
2025-02-26 16:55:59 -0600

    meeting 2-26-25
    experimented with writing mains. good talk but not much actual changes

---
+++ main.py
@@ -0,0 +1,13 @@
+
+
+# print("Initializing...")
+'''
+This is the main function for our OwlSat project, loosely based on / implementing [@TODO answer this] the pycubed library.
+
+'''
+
+if __name__ == "__main__":
+    # run some functions and stuff here that we define elsewhere
+
+    print("running function")
+
