39f9a64128f5185fc32934066fed0951474ffc45
rr-3A29 <rr82@rice.edu>
2025-03-05 17:06:07 -0600

    Updated main.py
    meeting 3-05-25

--- main.py
+++ main.py
@@ -1,13 +1,52 @@
 
-
-# print("Initializing...")
 '''
-This is the main function for our OwlSat project, loosely based on / implementing [@TODO answer this] the pycubed library.
+This is the main function for our OwlSat project, loosely based on / implementing [????]
+
+tasko: https://github.com/Rice-OwlSat/Flight-Software/blob/master/OwlSat%20-%20Flight%20Software/lib/tasko/
+--> loop.py is very helpful. Defines schedule and schedule later functions. It also runs the tasks.
 
 '''
 
-if __name__ == "__main__":
-    # run some functions and stuff here that we define elsewhere
+import os, tasko
+from pycubed import cubesat
+#cubesat is a new instance of the pycubed satellite
 
-    print("running function")
 
+#set everything to the same asyncio obj
+cubesat.tasko=tasko
+
+# Dict to store scheduled objects by name
+cubesat.scheduled_tasks={}
+
+for file in os.listdir('Tasks'): #loop through the tasks in the task folder
+    file = file[:-3] #remove the '.py' from file name
+
+    # ignore these files:
+    if file in ("") or file.startswith('._'):
+        continue
+
+    # auto-magically import the task file
+    # exec is literally running the code: import Tasks.file_name, importing the task file into main.
+    exec('import Tasks.{}'.format(file))
+
+    # create a helper object for scheduling the task
+    # It is running the code: task_obj = Tasks.file_name.task(cubesat), where cubesat is passed in for "self" in the task's code
+    task_obj = eval('Tasks.' + file).task(cubesat)
+
+    # schedule the task now or later based on its attributes
+    if hasattr(task_obj, 'schedule_later') and getattr(task_obj, 'schedule_later'):
+        schedule = cubesat.tasko.schedule_later
+    else:
+        schedule = cubesat.tasko.schedule
+
+    # schedule each task object and add it to our dict
+    cubesat.scheduled_tasks[task_obj.name] = schedule(task_obj.frequency, task_obj.main_task, task_obj.priority)
+
+#The print statement printing scheduled tasks.
+#NOTE --> This is the only use for cubesat.scheduled_tasks={}.
+#Prints everything it is going to do before actually running tasko
+print(len(cubesat.scheduled_tasks),'total')
+
+
+#start running the tasko loop
+cubesat.tasko.run()
