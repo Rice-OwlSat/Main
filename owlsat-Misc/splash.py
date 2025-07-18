b6dc3afabe15df16612bb7ca7556697e96bb4eeb
Viola-Case <ajr.acnt@gmail.com>
2025-07-18 14:26:36 -0400

    Color

--- owlsat-Misc/splash.py
+++ owlsat-Misc/splash.py
@@ -1,5 +1,10 @@
 import gc,time,sys,os,microcontroller
 
+RED = '\033[91m'
+GREEN = '\033[92m'
+BLUE = '\033[94m'
+RESET = '\033[0m'  
+
 def _format_uptime():
     seconds = int(time.monotonic())
     hours = seconds // 3600
@@ -23,16 +28,16 @@ def show_splash():
     try:
         with open("/splash", "r") as f:
             text = f.read()
-        print(text)
-        print("Platform:", sys.platform)
-        print("CircuitPython version:", os.uname().version)
-        print("Board:", os.uname().machine)
-        print("CPU:", os.uname().sysname)
+        print(f"{BLUE}{text}{RESET}")
+        print(f"{BLUE}Platform:{RESET}", sys.platform)
+        print(f"{BLUE}CircuitPython version:{RESET}", os.uname().version)
+        print(f"{BLUE}Board:{RESET}", os.uname().machine)
+        print(f"{BLUE}CPU:{RESET}", os.uname().sysname)
         gc.collect()
         max_ram = gc.mem_free() + gc.mem_alloc()
-        print("Free memory: ", gc.mem_free(), "/", max_ram, " bytes")
-        print("Uptime: ", _format_uptime())
-        print("CPU frequency:", microcontroller.cpu.frequency / 1000000, "MHz")
-        print("Disk:", get_storage_info())
+        print(f"{BLUE}Free memory:{RESET} ", gc.mem_free(), "/", max_ram, " bytes")
+        print(f"{BLUE}Uptime:{RESET}", _format_uptime())
+        print(f"{BLUE}CPU frequency:{RESET}", microcontroller.cpu.frequency / 1000000, "MHz")
+        print(f"{BLUE}Disk:{RESET}", get_storage_info())
     except Exception as e:
-        print("Splash error:", e)
+        print(f"{RED}Splash error:", e)
