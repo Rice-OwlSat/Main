cf247d40c9b3be7e112ab5ddeb14d8952503b956
Viola-Case <ajr.acnt@gmail.com>
2025-07-20 16:41:28 -0400

    Small change(s)
    Added some things for test board deployment

--- owlsat-Misc/splash.py
+++ owlsat-Misc/splash.py
@@ -1,9 +1,18 @@
 import gc,time,sys,os,microcontroller
 
+"""
+HOW TO USE:
+
+Import owlsat_fetch from splash and run it from main file (code.py)
+"""
+
+
 RED = '\033[91m'
 GREEN = '\033[92m'
 BLUE = '\033[94m'
-RESET = '\033[0m'  
+RESET = '\033[0m'
+
+VERSION = str(os.getenv("VERSION_MAJOR")) + '.' + str(os.getenv("VERSION_MINOR")) + '.' + str(os.getenv("VERSION_MICRO"))
 
 def _format_uptime():
     seconds = int(time.monotonic())
@@ -24,18 +33,19 @@ def get_storage_info():
 
     return f"{used / 1024:.2f} KiB / {total / 1024:.2f} KiB"
 
-def show_splash():
+def owlsat_fetch():
     try:
         with open("/splash", "r") as f:
             text = f.read()
         print(f"{BLUE}{text}{RESET}")
+        print(f"Rice University OwlSat software package version {VERSION}")
         print(f"{BLUE}Platform:{RESET}", sys.platform)
         print(f"{BLUE}CircuitPython version:{RESET}", os.uname().version)
         print(f"{BLUE}Board:{RESET}", os.uname().machine)
         print(f"{BLUE}CPU:{RESET}", os.uname().sysname)
         gc.collect()
         max_ram = gc.mem_free() + gc.mem_alloc()
-        print(f"{BLUE}Free memory:{RESET} ", gc.mem_free(), "/", max_ram, " bytes")
+        print(f"{BLUE}Free memory:{RESET} ", int(gc.mem_free() / 1024), "/", int(max_ram / 1024), " KiB")
         print(f"{BLUE}Uptime:{RESET}", _format_uptime())
         print(f"{BLUE}CPU frequency:{RESET}", microcontroller.cpu.frequency / 1000000, "MHz")
         print(f"{BLUE}Disk:{RESET}", get_storage_info())
